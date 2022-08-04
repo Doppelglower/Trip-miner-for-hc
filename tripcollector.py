935b3b2d336574f89829c2a211568dca0224c3eb
DPG <72010827+World-Administrator@users.noreply.github.com>
2022-08-04 19:42:59 +0800

    Add files via upload

--- tripcollector.py
+++ tripcollector.py
@@ -4,66 +4,68 @@ import os
 import threading
 import time
 import requests
-res = dict()
-room = "testttt"
+import sys
+room = "test"
+ini = 10.0
+thr = False
+rst = True
+def restartttt():
+    global rst,ini
+    rst = False
+    while ini>=0:
+        ini-=0.1
+        time.sleep(0.1)
+        if thr:
+            rst = True
+            return
+    restartt()
+restarttt = threading.Thread(target=restartttt)
+def restartt():
+	py=sys.executable
+	os.execl(py,py,*sys.argv)
 def cremin():
-    while True:
+    while thr:
         os.chdir(r"C:\Python310\mine")
         os.popen(r"python .\tripminer.py "+room)
-        time.sleep(30.0)
+        time.sleep(50.0)
+    return
+cre = threading.Thread(target=cremin)
 def getw(text,sender):
 	return str(text[len(sender)+12:])
 def on_open(ws):
     ws.send(json.dumps({"cmd": "join", "channel": room, "nick": "trip_collector"}))
-    cre = threading.Thread(target=cremin)
-    cre.start()
-
-    #socks4_list1 = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=10000&country=all&ssl=all&anonymity=all').text.split('\n')
-    #socks4_list2 = requests.get('https://www.proxyscan.io/download?type=socks4').text.split('\n')
-    #socks4_list3 = requests.get('https://www.proxy-list.download/api/v1/get?type=socks4').text.split('\n')
-    return 
-    socks4_list4 = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt').text.split()     
-    socks4_list5 = requests.get('https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies.txt').text.split()
-    socks4_list6 = requests.get('https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt').text.split()
-    socks4_list7 = requests.get('https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt').text.split()
-    socks4_list8 = requests.get('https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt').text.split()
-    socks4_list9 = requests.get('https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt').text.split()
-    socks4_list10 = requests.get('https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt').text.split()
-    socks4_list11 = requests.get('https://raw.githubusercontent.com/RX4096/proxy-list/main/online/socks4.txt').text.split()
-    socks4_list = list(set(socks4_list4 + socks4_list5 + socks4_list6 + socks4_list7 + socks4_list8 + socks4_list9 + socks4_list10 + socks4_list11))
-    #socks5_list1 = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all').text.split('\n')
-    #socks5_list2 = requests.get('https://www.proxyscan.io/download?type=socks5').text.split('\n')
-    #socks5_list3 = requests.get('https://www.proxy-list.download/api/v1/get?type=socks5').text.split('\n')
-    socks5_list4 = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt').text.split()
-    socks5_list5 = requests.get('https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt').text.split()
-    socks5_list6 = requests.get('https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt').text.split()
-    socks5_list7 = requests.get('https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt').text.split()
-    socks5_list8 = requests.get('https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt').text.split()
-    socks5_list9 = requests.get('https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt').text.split()
-    socks5_list10 = requests.get('https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt').text.split()
-    socks5_list11 = requests.get('https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt').text.split()
-    socks5_list12 = requests.get('https://raw.githubusercontent.com/ryanhaticus/superiorproxy.com/main/proxies.txt').text.split()
-    socks5_list13 = requests.get('https://raw.githubusercontent.com/RX4096/proxy-list/main/online/socks5.txt').text.split()
-    socks5_list = list(set(socks5_list4 + socks5_list5 + socks5_list6 + socks5_list7 + socks5_list8 + socks5_list9 + socks5_list10 + socks5_list11 + socks5_list12 + socks5_list13))
-    proxy_dict = {'socks4':socks4_list,'socks5':socks5_list}#'https':https_list}
-    proxy_list = socks4_list + socks5_list # + https_list
-    bb = open(r"C:\Python310\files\proxy.txt",mode="w")
-    bb.writelines(str(proxy_dict)+'\n')
-    bb.writelines(str(proxy_list))
-    bb.close()
-
+def clo():
+    global ini,thr,rst
+    while True:
+        thr = False
+        rst = False
+        ini -= 0.1
+        time.sleep(0.1)
+        if ini <= 0.0:
+            ws.send({})
+            sys.exit()
+cloo = threading.Thread(target=clo)
 def on_message(ws,message):
-    global res
+    global ini,thr,rst
     dat = json.loads(message)
     if dat["cmd"] == "info" and dat["type"] == "whisper":
         nic = dat["from"]
         tex = getw(str(dat["text"]),nic)
-        if str(nic).startswith("trip_miner_")  and dat["trip"] not in res:
-            res.update({dat["trip"]:tex})
-            aa = open(r"C:\Python310\files\trips.txt",mode="a")
-            aa.write(str(res)+'\n')
-            aa.close()
-            res.clear()
+        if str(nic).startswith("trip_miner_"):
+            with open(r"C:\Python310\files\trips.txt",mode="a") as aa:aa.write("{'"+dat["trip"]+"': '"+tex+"'}\n")
+            ini = 10.0
+    elif dat["cmd"] == "chat" and dat["trip"] in ["VEbeHK","AAfFKK"] and dat["text"] == "STOP":
+        thr = False
+        rst = False
+        cloo.start()
+    elif dat["cmd"] == "chat" and dat["trip"] in ["VEbeHK","AAfFKK"] and str(dat["text"]).lower() == "stop":
+        thr = False
+        if rst:
+            restarttt.start()
+    elif dat["cmd"] == "chat" and dat["trip"] in ["VEbeHK","AAfFKK"] and str(dat["text"]).lower() == "start":
+        thr = True
+        cre.start()
+
 if __name__ == "__main__":
     #websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://hack.chat/chat-ws",on_message=on_message)
--- tripminer.py
+++ tripminer.py
@@ -2,48 +2,29 @@ import json
 import time
 import random
 import threading
-import websocket
 from websocket import create_connection
 from websocket import WebSocketConnectionClosedException,WebSocketBadStatusException
 from ssl import SSLCertVerificationError, SSLEOFError
 from python_socks import ProxyConnectionError,ProxyError,ProxyTimeoutError
 import sys
 import ast
-# headers = {
-#     'authority': 'public.freeproxyapi.com',
-#     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"',
-#     'accept': 'application/octet-stream',
-#     'dnt': '1',
-#     'content-type': 'application/json',
-#     'sec-ch-ua-mobile': '?0',
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62',
-#     'sec-ch-ua-platform': '"Windows"',
-#     'origin': 'https://freeproxyapi.com',
-#     'sec-fetch-site': 'same-site',
-#     'sec-fetch-mode': 'cors',
-#     'sec-fetch-dest': 'empty',
-#     'referer': 'https://freeproxyapi.com/',
-#     'accept-language': 'zh-CN,zh;q=0.9',
-# }
-
-# data = '{"types":[],"levels":[],"countries":[],"type":"json","resultModel":"Mini"}'
-
-# response = requests.post('https://public.freeproxyapi.com/api/Download/Json', headers=headers, data=data)
 
 channel = str(sys.argv[1])
+proxy_list = list()
+proxy_dict = dict()
+i = 0
 
 def getDictKey_1(myDict, value):
     if value in myDict['socks4']:
         return 'socks4'
     if value in myDict['socks5']:
         return 'socks5'
-    # if value in myDict['https']:
-    #     return 'https'
+    if value in myDict['https']:
+        return 'https'
 
 def fuck():
-    global api,channel
     def shit():
-        global proxy_dict,i
+        global i
         try:
             ip = proxy_list.pop()
             ws=create_connection('wss://hack.chat/chat-ws',http_proxy_host=ip.split(':')[0], http_proxy_port=int(ip.split(':')[1]), proxy_type=getDictKey_1(proxy_dict,ip))
@@ -51,11 +32,12 @@ def fuck():
             pwd = ''.join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a','0','1','2','3','4','5','6','7','8','9','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'], random.randint(18,36)))
             req = json.dumps({"cmd": "join", "channel": channel, "nick": nam + "#" + pwd})
             ws.send(req)
-            time.sleep(0.2)
+            time.sleep(0.1)
             ws.send(json.dumps({"cmd":"chat","text":"/w trip_collector "+pwd}))
-            time.sleep(0.2)
             ws.close()
-        except (ProxyConnectionError,ProxyTimeoutError,ProxyError,SSLEOFError,SSLCertVerificationError,ValueError,WebSocketBadStatusException,WebSocketConnectionClosedException,ConnectionResetError,ConnectionRefusedError):
+            i = 0
+            return
+        except (ProxyConnectionError,ProxyTimeoutError,ProxyError,SSLEOFError,SSLCertVerificationError,ValueError,WebSocketBadStatusException,WebSocketConnectionClosedException,ConnectionResetError,ConnectionRefusedError,TimeoutError):
             i = 0
             return
         except BrokenPipeError:
@@ -64,42 +46,15 @@ def fuck():
     while True:
         shit()
         if i == 0:break
-        time.sleep(0.5)
+        time.sleep(3.0)
+    return
 
-# def get_zuan():
-#     url = random.choice(['https://api.shadiao.app/nmsl?level=min','https://api.shadiao.app/nmsl?level=max'])
-#     text = requests.get(url,timeout=5).text
-#     return json.loads(text)["data"]["text"]
-
-    # def getproxy():
-    #     d = {'Socks4':[],'Socks5':[],'Https':[]}
-    #     response = json.loads(requests.post('https://public.freeproxyapi.com/api/Download/Json', headers=headers, data=data).text)
-    #     for w in response:
-    #         if w['Type'] in ['Socks4','Socks5','Https']:
-    #             proxyip = w['Host'] + ':' + str(w['Port'])
-    #             d[w['Type']].append(proxyip)
-    #     return d
-        
-    # m = getproxy()
 def main():
-    global proxy_list,proxy_dict,thread_count,success_count
+    global proxy_list,proxy_dict
     thread_list = []
-    thread_count = 0
-    success_count = 0
-    proxy_lst = []
-    lst = []
-#     https_list1 = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=10000&country=all&ssl=all&anonymity=all').text.split('\r\n')
-#     https_list2 = requests.get('https://www.proxyscan.io/download?type=https').text.split('\n')
-#     https_list3 = requests.get('https://www.proxy-list.download/api/v1/get?type=https').text.split('\r\n')
-# #    https_list4 = requests.get('https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt').text.split('\n')
-#     https_list5 = requests.get('https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt').text.split('\n')
-#     https_list6 = requests.get('https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt').text.split('\n')
-#     https_list7 = requests.get('https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/https.txt').text.split('\n')
-#     https_list8 = m['Https']
-#     https_list = list(set(https_list1 + https_list2 + https_list3 + https_list5 + https_list6 + https_list7 + https_list8))
-#     print(len(https_list))
     aa = open(r"C:\Python310\files\proxy.txt",mode="r")
     bb = aa.readlines()
+    aa.close()
     proxy_dict = ast.literal_eval(bb[0][:-1])
     proxy_list = ast.literal_eval(bb[1])
 
